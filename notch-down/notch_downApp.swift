f56627ca374f79b26e389fe1d8c6363483aa7f50
Festo Magembe <festomanolo@gmail.com>
2026-01-22 10:45:43 +0300

    Initial Commit

---
+++ notch-down/notch_downApp.swift
@@ -0,0 +1,17 @@
+//
+//  notch_downApp.swift
+//  notch-down
+//
+//  Created by festomanolo on 22/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct notch_downApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
